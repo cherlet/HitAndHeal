80a13da05dd175226ebbccc0d9c6d25bd5d89889
justUzm <uzmel@icloud.com>
2023-10-03 05:31:47 +0300

    Initial Commit

---
+++ HeatAndHeal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeatAndHeal
+//
+//  Created by Усман Махмутхажиев on 03.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
